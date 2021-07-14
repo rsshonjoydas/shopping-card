325b8ab06c0bff3a4226362fe40930b969034454
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-14 06:16:38 +0600

    log api data in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
-import "./styles.css";
+// ? internal imports
+// ? Components imports
+import { useQuery } from "react-query";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
+
   return (
     <>
       <h1>Shopping Cart</h1>
